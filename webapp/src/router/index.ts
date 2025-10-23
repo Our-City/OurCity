bf799e7764b94635a2bb134ce7e164f29a3e23c5
Will Reimer <willcreimer@gmail.com>
2025-10-22 19:14:16 -0500

    header and profile view

--- webapp/src/router/index.ts
+++ webapp/src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
 import PageDetailView from "@/views/PageDetailView.vue";
+import ProfileView from "@/views/ProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "home", component: HomeView },
+    { path: "/profile", name: "profile", component: ProfileView },
     { path: "/posts/:id", name: "post-detail", component: PageDetailView },
   ],
 });
