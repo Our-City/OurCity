57dd71266b9f1f67f90c79bd15aa8d81ebe024a2
WillReimer <reimerw1@myumanitoba.ca>
2025-10-24 13:18:03 -0500

    profile page setup

---
+++ webapp/src/components/profile/ProfileHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="profile-header">
+    <span><i class="pi pi-user" /><h1 class="profile-username">Username</h1></span>
+
+  </div>
+</template>
+
+<style scoped>
+.profile-header {
+  background: var(--primary-background-color);
+  color: var(--primary-text-color);
+  padding: 1rem;
+  border-bottom: 1px solid var(--border-color, #ccc);
+}
+
+.profile-username {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+</style>
