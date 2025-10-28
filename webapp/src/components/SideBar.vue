862eb72389338690c062725b830682cfef95bb9f
Will Reimer <willcreimer@gmail.com>
2025-10-27 19:55:49 -0500

    sidebar

--- webapp/src/components/SideBar.vue
+++ webapp/src/components/SideBar.vue
@@ -1,8 +1,60 @@
 <script setup lang="ts">
+    import { useRouter } from "vue-router";
+    import { usePostFilters } from "@/composables/usePostFilters";
+    
+    interface Props {
+        view?: 'home' | 'profile';
+    }
+
+    const router = useRouter();
+    const { setSort, setFilter, reset } = usePostFilters();
+
+    const props = withDefaults(defineProps<Props>(), {
+        view: 'home'
+    })
+
+    function goToHome(): void {
+        reset();
+        router.push("/");
+    }
+
+    function handleSort(sortType: 'popular' | 'nearby' | 'recent') {
+        setSort(sortType);
+    }
+
+    function handleFilter(filterType: 'recreational' | 'infrastructure' | 'all') {
+        setFilter(filterType);
+    }
+
 </script>
 
 <template>
+    <div :class="['side-bar', `toolbar--${props.view}`]">
+        <button class="home-button" @click="goToHome"><i class="pi pi-home"></i> Home</button>
+        <button class="popular-button" @click="reset(); handleSort('popular')"><i class="pi pi-chart-line"></i> Popular</button>
+        <button class="nearby-button" @click="reset(); handleSort('nearby')"><i class="pi pi-map-marker"></i> Nearby</button>
+        <button class="recreational-button" @click="reset(); handleFilter('recreational')"><i class="pi pi-sun"></i> Recreational</button>
+        <button class="infrastructure-button" @click="reset(); handleFilter('infrastructure')"><i class="pi pi-building"></i> Infrastructure</button>
+    </div>
 </template>
 
 <style scoped>
+
+    .side-bar {
+        display: flex; 
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start; 
+        width: 20rem; 
+        padding: 1rem;
+        height: 100%; 
+        background: var(--primary-background-color); 
+    }
+
+    .side-bar button {
+        width: 100%;
+        text-align: left;
+        display: flex;
+        gap: 0.7rem;
+    }
 </style>
