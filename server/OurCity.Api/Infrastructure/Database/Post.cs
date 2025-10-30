15bab04975e7553720066154532b375035ea8a55
Jade Lee <jadelee0924@gmail.com>
2025-10-29 21:12:20 -0500

    Update Post entity to match new detailed contract

--- server/OurCity.Api/Infrastructure/Database/Post.cs
+++ server/OurCity.Api/Infrastructure/Database/Post.cs
@@ -1,25 +1,33 @@
+using OurCity.Api.Common.Enum;
+
 namespace OurCity.Api.Infrastructure.Database;
 
 public class Post
 {
     public Guid Id { get; set; }
+    
+    public Guid AuthorId { get; set; }  
 
     public required string Title { get; set; }
 
     public required string Description { get; set; }
 
-    public User Author { get; set; } = null!;
-    public Guid AuthorId { get; set; }
-
     public string? Location { get; set; }
 
-    public List<Guid> UpvotedUserIds { get; set; } = new();
-    public List<Guid> DownvotedUserIds { get; set; } = new();
+    public PostVisibility Visisbility { get; set; } = PostVisibility.Published;
+
+    public List<Tag> Tags { get; set; } = new();
+
+    public List<PostVote> Votes { get; set; } = new();
+
+    public bool IsDeleted { get; set; }
 
     public DateTime CreatedAt { get; set; }
+
     public DateTime UpdatedAt { get; set; }
 
-    public List<Image> Images { get; set; } = new();
-
-    public List<Comment> Comments { get; set; } = new();
+    // Navigation Property
+    public User? Author { get; set; }
+    
+    public List<Comment>? Comments { get; set; }
 }
