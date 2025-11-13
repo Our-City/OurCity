bc73998aa638b9b4209cd7eda428daf23fc5ec95
Andre <106368574+seigfrieds@users.noreply.github.com>
2025-11-13 16:00:53 -0600

    Fix regex (#129)

--- server/OurCity.Api/Common/Dtos/Post/PostCreateRequestDto.cs
+++ server/OurCity.Api/Common/Dtos/Post/PostCreateRequestDto.cs
@@ -10,7 +10,7 @@ public class PostCreateRequestDto
         MinimumLength = 1,
         ErrorMessage = "Title must be between 1 and 50 characters"
     )]
-    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Title cannot be only whitespace")]
+    [RegularExpression(@"^\s*\S.*$", ErrorMessage = "Title cannot be only whitespace")]
     public required string Title { get; set; }
 
     [Required(ErrorMessage = "Description is required")]
@@ -19,7 +19,7 @@ public class PostCreateRequestDto
         MinimumLength = 1,
         ErrorMessage = "Description must be between 1 and 500 characters"
     )]
-    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Description cannot be only whitespace")]
+    [RegularExpression(@"^\s*\S[\s\S]*$", ErrorMessage = "Description cannot be only whitespace")]
     public required string Description { get; set; }
 
     [StringLength(50, ErrorMessage = "Location cannot exceed 50 characters")]
--- server/OurCity.Api/Common/Dtos/Post/PostUpdateRequestDto.cs
+++ server/OurCity.Api/Common/Dtos/Post/PostUpdateRequestDto.cs
@@ -10,7 +10,7 @@ public class PostUpdateRequestDto
         MinimumLength = 1,
         ErrorMessage = "Title must be between 1 and 50 characters"
     )]
-    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Title cannot be only whitespace")]
+    [RegularExpression(@"^\s*\S.*$", ErrorMessage = "Title cannot be only whitespace")]
     public string? Title { get; set; }
 
     [StringLength(
@@ -18,7 +18,7 @@ public class PostUpdateRequestDto
         MinimumLength = 1,
         ErrorMessage = "Description must be between 1 and 500 characters"
     )]
-    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Description cannot be only whitespace")]
+    [RegularExpression(@"^\s*\S[\s\S]*$", ErrorMessage = "Description cannot be only whitespace")]
     public string? Description { get; set; }
 
     [StringLength(50, ErrorMessage = "Location cannot exceed 50 characters")]
